2bdca69fa23b290ede40e87345483034f48e7f5c
alexjoham <alexjoham@icloud.com>
2021-03-05 18:18:14 +0100

    added option to customize the welcome message

--- src/de/alexjoham/ServerPlugin/JoinMessage.java
+++ src/de/alexjoham/ServerPlugin/JoinMessage.java
@@ -7,8 +7,42 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinMessage implements Listener {
 
+    private String message;
+    private ChatColor color;
+
+    public JoinMessage(String message, String color) {
+        this.message = message;
+        this.color = ChatColor.valueOf(color);
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        e.setJoinMessage(ChatColor.GREEN + "Wilkommen zurück, " + e.getPlayer().getName() + "!");
+        e.setJoinMessage(color + getCastedMessage(e));
+    }
+
+    public String getCastedMessage(PlayerJoinEvent e) {
+        if (!message.contains("{"))
+            return message;
+        StringBuilder result = new StringBuilder();
+        while (message.contains("{")) {
+            if (!message.contains("}")) {
+                break;
+            }
+            result.append(message, 0, message.indexOf("{")).append(getValueOf(message.substring(message.indexOf("{") + 1, message.indexOf("}")), e));
+            message = message.substring(message.indexOf("}") + 1);
+            if (message.length() == 0)
+                break;
+        }
+        result.append(message);
+        return result.toString();
+    }
+
+    public String getValueOf(String code, PlayerJoinEvent e) {
+        switch (code) {
+            case "playername":
+                return e.getPlayer().getName();
+            default:
+                return "Unknown code";
+        }
     }
 }
--- src/de/alexjoham/ServerPlugin/ServerPlugin.java
+++ src/de/alexjoham/ServerPlugin/ServerPlugin.java
@@ -1,15 +1,26 @@
 package de.alexjoham.ServerPlugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerPlugin extends JavaPlugin {
 
 
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new JoinMessage(), this);
+        loadConfigFile();
+        JoinMessage joinMessage = new JoinMessage(this.getConfig().getString("defaultWelcomeMessage"), this.getConfig().getString("defaultWelcomeMessageColor"));
+        getServer().getPluginManager().registerEvents(joinMessage, this);
     }
 
     public void onDisable() {
 
+    }
+
+    private void loadConfigFile() {
+        FileConfiguration configuration = this.getConfig();
+        configuration.addDefault("defaultWelcomeMessage", "Welcome back, {playername}!");
+        configuration.addDefault("defaultWelcomeMessageColor", "GREEN");
+        configuration.options().copyDefaults(true);
+        saveConfig();
     }
 }
