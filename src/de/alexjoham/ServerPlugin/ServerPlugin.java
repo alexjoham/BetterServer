45759ea43e4c89fe4a0062f048e2294e8dd30e5f
BuildTools <unconfigured@null.spigotmc.org>
2021-03-05 15:17:44 +0100

    Initial commit

---
+++ src/de/alexjoham/ServerPlugin/ServerPlugin.java
@@ -0,0 +1,13 @@
+package de.alexjoham.ServerPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerPlugin extends JavaPlugin {
+    public void onEnable() {
+        System.out.println("Starting plugin!");
+    }
+
+    public void onDisable() {
+
+    }
+}
