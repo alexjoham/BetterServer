6e365f9522bab36e2d0b5ee7bac91b67d2e424fe
Alexander Joham <alexjoham@icloud.com>
2021-03-07 12:26:56 +0100

    Added Startup message added option for less needed sleepers

--- src/de/alexjoham/ServerPlugin/ServerPlugin.java
+++ src/de/alexjoham/ServerPlugin/ServerPlugin.java
@@ -1,7 +1,10 @@
 package de.alexjoham.ServerPlugin;
 
+
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class ServerPlugin extends JavaPlugin {
 
@@ -10,11 +13,11 @@ public class ServerPlugin extends JavaPlugin {
         loadConfigFile();
         getServer().getPluginManager().registerEvents(new JoinMessage(this.getConfig().getString("defaultWelcomeMessage"), this.getConfig().getString("defaultWelcomeMessageColor")), this);
         getServer().getPluginManager().registerEvents(new SkipNightAtNumberOfSleepers(getServer(), this.getConfig().getString("playerNeededToSleep")), this);
-
+        getServer().getConsoleSender().sendMessage("[BetterServer] " + ChatColor.BLUE + "plugin has been successfully enabled");
     }
 
     public void onDisable() {
-
+        getServer().getConsoleSender().sendMessage("[BetterServer] " + ChatColor.BLUE + "plugin has been successfully disabled");
     }
 
     /**
--- src/de/alexjoham/ServerPlugin/SkipNightAtNumberOfSleepers.java
+++ src/de/alexjoham/ServerPlugin/SkipNightAtNumberOfSleepers.java
@@ -1,37 +1,83 @@
 package de.alexjoham.ServerPlugin;
 
-import org.apache.logging.log4j.Level;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.*;
 
 public class SkipNightAtNumberOfSleepers implements Listener {
 
     private Server server;
     private String playerNeededToSleep;
+    private HashMap<World, List<Player>> sleepingPlayers;
 
     public SkipNightAtNumberOfSleepers(Server server, String playerNeededToSleep) {
         this.server = server;
         this.playerNeededToSleep = playerNeededToSleep;
+        sleepingPlayers = new HashMap<>();
     }
-
-    private int counter = 0;
 
     @EventHandler
     public void onPlayerLyingDown(PlayerBedEnterEvent e) {
-        if (e.getBedEnterResult().compareTo(PlayerBedEnterEvent.BedEnterResult.OK) == 0)
-            counter++;
+
+        World world = e.getPlayer().getWorld();
+        if (e.getBedEnterResult().compareTo(PlayerBedEnterEvent.BedEnterResult.OK) == 0) {
+            List<Player> list = sleepingPlayers.get(e.getPlayer().getWorld());
+            if (list == null) {
+                list = new ArrayList<>();
+            }
+            list.add(e.getPlayer());
+            sleepingPlayers.put(e.getPlayer().getWorld(), list);
+        }
         if (playerNeededToSleep.equals("MAX")) {
-            if (counter == server.getOnlinePlayers().stream().filter(entry -> !entry.getPlayer().getWorld().getName().contains("_nether")).toArray().length)
+            if (sleepingPlayers.get(world).size() == server.getOnlinePlayers().stream().filter(entry -> !world.getName().contains("_nether")).toArray().length)
                 e.getPlayer().getWorld().setTime(1000);
-        } else if(playerNeededToSleep.contains("%")) {
+        } else if (playerNeededToSleep.contains("%")) {
             int percent = 0;
             try {
-                percent = Integer.parseInt(playerNeededToSleep.substring(0, playerNeededToSleep.length()-2));
-            } catch(NumberFormatException exception) {
-                //server.getLogger().logp(Level.ERROR,);
+                percent = Integer.parseInt(playerNeededToSleep.substring(0, playerNeededToSleep.length() - 2));
+            } catch (NumberFormatException exception) {
+                server.getLogger().logp(Level.SEVERE, this.toString(), "onPlayerLyingDown(PlayerBedEnterEvent e)", "Integer excpected, but was: " + playerNeededToSleep.substring(0, playerNeededToSleep.length() - 2), new Throwable(exception.getCause()));
             }
+            numberIsNull(e, percent);
+        } else {
+            int number = 0;
+            try {
+                number = Integer.parseInt(playerNeededToSleep);
+            } catch (NumberFormatException exception) {
+                server.getLogger().logp(Level.SEVERE, this.toString(), "onPlayerLyingDown(PlayerBedEnterEvent e)", "Integer excpected, but was: " + playerNeededToSleep.substring(0, playerNeededToSleep.length() - 2), new Throwable(exception.getCause()));
+            }
+            numberIsNull(e, number);
         }
+    }
+
+    private void numberIsNull(PlayerBedEnterEvent e, int number) {
+        World world = e.getPlayer().getWorld();
+        if (number != 0) {
+            int sleeping = e.getPlayer().getWorld().getPlayers().stream().filter(OfflinePlayer::isOnline).toArray().length / sleepingPlayers.get(e.getPlayer().getWorld()).size();
+            if (sleeping >= number) {
+                e.getPlayer().getWorld().setTime(1000);
+            }
+        } else {
+            if (sleepingPlayers.get(world).size() == server.getOnlinePlayers().stream().filter(entry -> !entry.getPlayer().getWorld().getName().contains("_nether")).toArray().length)
+                e.getPlayer().getWorld().setTime(1000);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerStandingUp(PlayerBedLeaveEvent e) {
+        List<Player> list;
+        list = sleepingPlayers.get(e.getPlayer().getWorld());
+        list.remove(e.getPlayer());
+        sleepingPlayers.put(e.getPlayer().getWorld(), list);
     }
 }
